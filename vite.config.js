0f76d5519d548e88cab59dfa8b9e20d211e790fa
TestRoverAutomation <testroverautomation@gmail.com>
2024-10-13 22:00:32 +0100

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['firebase/app'] // Exclude firebase/app and firebase/auth from the build
-    },
-  },
+  optimizeDeps: {
+    include: ["firebase/auth"]
+  }
 });
