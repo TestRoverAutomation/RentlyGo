6e3820a78246f35d87faa4d49687eb3c2deead0f
TestRoverAutomation <testroverautomation@gmail.com>
2025-02-13 13:41:39 +0000

    Added slider to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,12 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import image1 from "../images/hero_images/RentlyGo2.jpeg";
-import image2 from "../images/hero_images/appartment.jpeg"
-import image3 from "../images/hero_images/bikes.jpeg"
-import image4 from "../images/hero_images/dress.jpeg"
-import image5 from "../images/hero_images/RentlyGoMan.jpeg"
-import image6 from "../images/hero_images/RentlyGoMan2.jpeg"
-import image7 from "../images/hero_images/RentlyGoWoman.jpeg"
+import image2 from "../images/hero_images/appartment.jpeg";
+import image3 from "../images/hero_images/bikes.jpeg";
+import image4 from "../images/hero_images/dress.jpeg";
+import image5 from "../images/hero_images/RentlyGoMan.jpeg";
+import image6 from "../images/hero_images/RentlyGoMan2.jpeg";
+import image7 from "../images/hero_images/RentlyGoWoman.jpeg";
 import {
   FaHome,
   FaTshirt,
@@ -31,7 +31,6 @@ const sliderSettings = {
   autoplaySpeed: 3000,
   arrows: false,
 };
-
 
 const categories = [
   { name: "Properties", icon: <FaHome />, path: "/properties" },
@@ -62,15 +61,17 @@ const Home = () => {
       {/* Hero Slider */}
       <section className="relative overflow-hidden">
         <Slider {...sliderSettings}>
-          {[image1, image2, image3, image5, image4, image7 ].map((img, index) => (
-            <div key={index}>
-              <img
-                src={img}
-                alt={`Hero ${index + 1}`}
-                className="w-full h-auto max-h-[500px] object-cover"
-              />
-            </div>
-          ))}
+          {[image1, image2, image3, image5, image4, image7].map(
+            (img, index) => (
+              <div key={index}>
+                <img
+                  src={img}
+                  alt={`Hero ${index + 1}`}
+                  className="w-full h-auto max-h-[500px] object-cover"
+                />
+              </div>
+            ),
+          )}
         </Slider>
       </section>
 
